3b6e8c06d2c7b5248ad6d87a2e5e742f4a20a913
saleemsheikh <sheikhsaleem407@gmail.com>
2017-07-12 00:19:00 +0530

    Add files via upload
    This is my first project that i have completed in my Master Degree.

---
+++ iam-saleem/src/fr/epita/iam/configuration/Configuration.java
@@ -0,0 +1,31 @@
+package fr.epita.iam.configuration;
+/**
+ * @author saleem
+ */
+public class Configuration {
+	/**
+     * JDBC Connection string.
+     */
+    public static final String jdbcURL = "jdbc:derby://localhost:1527/saleem";
+
+    /**
+     * JDBC Username to get connection.
+     */
+    public static final String jdbcUsername = "admin";
+
+    /**
+     * JDBC Password to get connection
+     */
+    public static final String jdbcPassword = "admin";
+
+    /**
+     * Authenticator Username.
+     */
+    public static final String authenticateUsername = "admin";
+
+    /**
+     * Authenticator Password.
+     */
+    public static final String authenticatePassword = "admin";
+
+}
---
+++ iam-saleem/src/fr/epita/iam/exceptions/DAOCreateException.java
@@ -0,0 +1,22 @@
+package fr.epita.iam.exceptions;
+/**
+ * @author saleem
+ */
+public class DAOCreateException extends Exception {
+	 private Object faultObject;
+
+	    /**
+	     *
+	     * @param Object
+	     */
+	    public void setFaultObject(Object obj){
+	        this.faultObject = obj;
+	    }
+
+	       @Override
+	    public String getMessage() {
+	        return super.getMessage() + String.valueOf(this.faultObject);
+	    }
+	}
+
+
